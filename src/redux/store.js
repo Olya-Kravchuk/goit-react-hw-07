1ae7854264d03961aff1ea93d0ce57d5507261d0
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2024-04-24 11:18:26 +0200

    ht

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,14 @@
 
 import { filtersReducer } from "./filtersSlice";
 import { contactsReducer } from "./contactsSlice";
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-import { configureStore } from "@reduxjs/toolkit";
 
-// const contactPersistConfig = {
-//   key: "contacts",
-//   storage,
-//   whitelist: ["items"],
-// };
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
-    // contacts: persistReducer(contactPersistConfig, contactsReducer),
     contacts: contactsReducer,
     filters: filtersReducer,
   },
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
+
 });
 
-// export const persistor = persistStore(store);
